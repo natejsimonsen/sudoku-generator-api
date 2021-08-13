e642f63c15187b1a4f6ff739c956001aec598a0f
Nate Simonsen <nate@khoza.com>
2021-08-13 13:55:56 -0700

    heroku-deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const init = require("./sudoku");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.get("/", (req, res) => res.send(JSON.stringify(init())));
